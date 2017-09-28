e2de139903e924101460b5b59035d5ccf2d481f1
irow0433 <31513820+irow0433@users.noreply.github.com>
2017-09-28 09:06:31 -0600

    added loops

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,13 +1,17 @@
 package monster.controller;
 
+import java.util.List;
+
 //import java.util.Scanner;
 //import java.util.Scanner;
 
 import monster.model.MarshmallowMonster;
 import monster.view.MonsterDisplay;
 
-public class MonsterController 
+public class MonsterController
 {
+private List<MarshmallowMonster> monsterlist;
+
 	private MonsterDisplay popup;
 	
 	public MonsterController()
@@ -16,6 +20,11 @@ public class MonsterController
 	}
 
 	public void start() {
+		boolean finished = true;
+		while(finished);
+		{
+			popup.displayText("I am so neat");
+		}
 	}
 	
 	private void interactWithTheMonster1(MarshmallowMonster currentMonster) {
@@ -39,23 +48,6 @@ public class MonsterController
 		return valid;
 	}
 	
-		MarshmallowMonster sample = new MarshmallowMonster();
-//		Systemout.println(sample);
-		System.out.println(sample);
-		MarshmallowMonster realMonster = new MarshmallowMonster("Scary", 8, 2, 3, true);
-		
-		//System.out.println(realMonster);
-		popup.displayText(realMonster.toString());
-		//System.out.println("Isaac is very hungry, so he will eat one tentacle");
-		popup.displayText("Isaac is very hungry, so he will eat one tentacle");
-		realMonster.setTentacleAmount(2);
-		//System.out.println(realMonster);
-		popup.displayText(realMonster.toString());
-		void interactWithTheMonster1(realMonster);
-		
-		interactWithTheMonster1(realMonster);
-	}
-	
 	private void interactWithTheMonster(MarshmallowMonster currentMonster)
 	{	
 		popup.displayText("Hey look at me!");
@@ -72,72 +64,91 @@ public class MonsterController
 		int specialAnswer = Integer.parseInt(answer);
 		String unconverted = popup.getResponse("How many do you want to eat");
 		
-		if(isValidInteger(unconverted))
-		{
-			specialAnswer = Integer.parseInt(unconverted);
-		}
+		MarshmallowMonster realMonster = null;
+		monsterlist.add(realMonster);
+		MarshmallowMonster sample = null;
+		monsterlist.add(sample);
+		testlist1();
+	}
+			private void testlist1()
+			{
+				for(int index = 0; index < monsterlist.size(); index++);
+				{
+					int index = 0;
+					MarshmallowMonster currentMonster = monsterlist.get(index);
+					popup.displayText(currentMonster.getName());
+					String newName = popup.getResponse("what should my new name be??");
+					currentMonster.setNaame(newName);
+					popup.displayText(currentMonster.getName());
+				}
+				
+				for (int i = 0; i < monsterlist.size(); i++) {
+					MarshmallowMonster current = monsterlist.get(i);
+					{
+						popup.displayText(current.getName());
+						String newName = popup.getResponse("what wouldl my new new name be");
+						current.setNaame(newName);
+						popup.displayText(current.getName());
+					}}}
+		
+		//while(!isValidInteger(unconverted))
+		//{
+		//	popup.displayText("try again!");
+		//	unconverted = popup.getResponse("How many arms???");
+		//}
+		
+		//if(isValidInteger(unconverted))
+		//{
+		//	specialAnswer = Integer.parseInt(unconverted);
+		//}
 		
 		//int consumed = myScanner.nextInt();
 		//int difconsumed = myScanner.nextInt();
 		
-		//if(consumed< 0)
-		{
-			System.out.println("You cannot eat a negative number silly human");
-		}
-		//else if (consumed == 0)
-		{
-			System.out.println("why would you want to eat nothing dummy?");
-		}
-		//else if(consumed> currentMonster.getArmCount());
-		{
-			System.out.println("That is impossible - I only have " + currentMonster.getArmCount() + " arms!!!!");
-		}
-		// if(consumed< currentMonster.getArmCount())
-		{
-		//	currentMonster.setArmCount(currentMonster.getArmCount() - consumed);
-			System.out.println("Thank you so much! I only hae this many arms now. " +currentMonster.getArmCount());
-		}
-	{
-		System.out.println(currentMonster.getName() + " suggests tentacles they have " + currentMonster.getTentacleAmount());
-		System.out.println("How many do you want to eat?");
+//		if(consumed< 0)
+//		{
+//			System.out.println("You cannot eat a negative number silly human");
+//		}
+//		else if (consumed == 0)
+//		{
+//			System.out.println("why would you want to eat nothing dummy?");
+//		}
+//		else if(consumed> currentMonster.getArmCount());
+//		{
+//			System.out.println("That is impossible - I only have " + currentMonster.getArmCount() + " arms!!!!");
+//		}
+//		 if(consumed< currentMonster.getArmCount())
+//		{
+//			currentMonster.setArmCount(currentMonster.getArmCount() - consumed);
+//			System.out.println("Thank you so much! I only hae this many arms now. " +currentMonster.getArmCount());
+//		}
+//	{
+//		System.out.println(currentMonster.getName() + " suggests tentacles they have " + currentMonster.getTentacleAmount());
+//		System.out.println("How many do you want to eat?");
+//		
+//	}
+//		if(consumed< 0) 
+//		{
+//			System.out.println("You cannot eat a negative number silly human");
+//		}
+//		else if (consumed == 0)
+//		{
+//			System.out.println("Why would you want to eat nothing silly willy");
+//		}
+//		else if(consumed> currentMonster.getTentacleAmount());
+//		{
+//			System.out.println("This is not possible - I only have " + currentMonster.getTentacleAmount());
+//		}
+//		if(consumed< currentMonster.getTentacleAmount())
+//		{
+//			currentMonster.setTentacleAmount(currentMonster.getTentacleAmount);
+//			currentMonster.setTentacleAmount("Thank you very much! I only have this many Tentacles now." +currentMonster.getArmCount);
+//		}}
+//
+	private void testlist() {
+//		 TODO Auto-generated method stub
 		
-	}
-		//if(consumed< 0) 
-		{
-			System.out.println("You cannot eat a negative number silly human");
-		}
-		//else if (consumed == 0)
-		{
-			System.out.println("Why would you want to eat nothing silly willy");
-		}
-		//else if(consumed> currentMonster.getTentacleAmount());
-		{
-			System.out.println("This is not possible - I only have " + currentMonster.getTentacleAmount());
-		}
-		//if(consumed< currentMonster.getTentacleAmount())
-		{
-			currentMonster.setTentacleAmount(currentMonster.getTentacleAmount);
-			currentMonster.setTentacleAmount("Thank you very much! I only have this many Tentacles now." +currentMonster.getArmCount);
-		}}
-		private boolean isValidDouble(String sampleDouble)
-		{
-			boolean valid = false;
-			
-			
-		
-		catch(NumberFormatException error):
-		{
-			String sampleDouble;
-			popup.displayText("Only double values are valid: " + sampleDouble + " is not.");
-			{
-		
-		Object valid;
-		return valid;
-	
-				
-		
-		
-	}
-}}}
+	}	
+}
 
 	
